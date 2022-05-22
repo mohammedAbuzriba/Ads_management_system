0cc6740af919a701465b9c35a98426643921e891
moham <mohamedabuzriba@hotmail.com>
2022-05-22 19:00:05 +0200

    google login

--- accounts/url.py
+++ accounts/url.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from django.contrib.auth import views as auth_views
 
@@ -9,6 +9,6 @@ urlpatterns = [
     path('login/',auth_views.LoginView.as_view(template_name='login.html'),name='login'),
     path('changePassword/',auth_views.PasswordChangeView.as_view(template_name='changePassword.html'),name='changePassword'),
     path('change_password/done/',auth_views.PasswordChangeDoneView.as_view(template_name='changePasswordDone.html'),name='password_change_done'),
-    path('account/',views.UserUpdateview.as_view(),name='myAccount')
-
+    path('account/',views.UserUpdateview.as_view(),name='myAccount'),
+    path('accounts/', include('allauth.urls')),
 ]
