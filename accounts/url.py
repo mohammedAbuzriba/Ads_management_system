6f51979866262d3315d2d21c33d4693b9c9773b9
moham <mohamedabuzriba@hotmail.com>
2022-05-24 13:34:47 +0200

    last update

--- accounts/url.py
+++ accounts/url.py
@@ -1,4 +1,4 @@
-from django.urls import path, include, re_path
+from django.urls import path, include
 from . import views
 from django.contrib.auth import views as auth_views
 
@@ -12,7 +12,6 @@ urlpatterns = [
     path('account/',views.UserUpdateview.as_view(),name='myAccount'),
     path('accounts/', include('allauth.urls')),
 
-    re_path(r'web/', include('django.contrib.auth.urls')),
     # path('reset_password/',auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"),name="reset_password"),
     # path('reset_password_sent/',auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_sent.html"),name="password_reset_done"),
     # path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_form.html"),name="password_reset_confirm"),
